b40c5dab11ce0fb25cbb1c748abbdbd50e6f6f52
IndexAsm <kubagraa111@gmail.com>
2026-08-23 15:59:53 +0200

    Fixed .gitignore

--- build/linux/debug/dep/src/core/Debug/DebugWindow.d
+++
@@ -1,9 +0,0 @@
-build/linux/debug/obj/src/core/Debug/DebugWindow.o: \
- src/core/Debug/DebugWindow.cpp src/core/Debug/DebugWindow.h \
- libraries/imgui/include/imgui.h libraries/imgui/include/imconfig.h \
- src/pch.hpp libraries/LanguageManager/include/LanguageManager.h
-src/core/Debug/DebugWindow.h:
-libraries/imgui/include/imgui.h:
-libraries/imgui/include/imconfig.h:
-src/pch.hpp:
-libraries/LanguageManager/include/LanguageManager.h:
--- build/linux/debug/dep/src/core/Window.d
+++
@@ -1,5 +0,0 @@
-build/linux/debug/obj/src/core/Window.o: src/core/Window.cpp \
- src/core/Window.h libraries/glfw/include/GLFW/glfw3.h src/pch.hpp
-src/core/Window.h:
-libraries/glfw/include/GLFW/glfw3.h:
-src/pch.hpp:
